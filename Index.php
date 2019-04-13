0e0e20d2eb111c0d24b89839418fd8a5d2cc0919
zebra202 <zebra.202@hotmail.com>
2019-04-13 23:28:43 +0200

    Inicijalni commit

---
+++ Index.php
@@ -0,0 +1,97 @@
+<DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Embreuš Photography</title>
+		<!--CSS-->
+		<!--Bootstrap-->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	
+		<!--	 Font awsome -->		
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">		
+		<!-- Blma CSS -->
+		<link rel="stylesheet" href="css/bulma.min.css">
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css">		
+</head>	
+<body>
+	<!-- HEADER -->
+	<header>
+		<div class='container-fluid'>
+			<div class='jumbotron text-center'>
+				<h1>Embreuš Photography</h1>
+				<p>Amater photography site<p>
+			</div>
+		</div>
+	
+	</header>
+	<nav class='navbar navbar-inverse navbar-fixed-top'>
+		<div class='container-fluid'>
+			<header class='navbar-header'>
+				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+				</button>
+				<a class='navbar-brand' href='index.php'>Home page</a>
+			</header>
+			<div class='collapse navbar-collapse' id='myNavbar'>
+			<ul class='nav navbar-nav'>
+				<li><a href='#'>Portfolio</a></li>
+				<li><a href='#'>Conctact</a></li>
+				<li><a href='#'>About</a></li>
+			</ul>
+			
+			<ul class='nav navbar-nav navbar-right'>
+			<li>
+				<form class='navbar-form' action='#' method='POST'>
+					<input type='text' class='form-control' placeholder='Search the site'>
+					<button type='submit' class='btn-primary'>
+					<span class='glyphicon glyphicon-search'></span>
+					
+					</button>
+			</li>
+			</ul>
+		</div>			
+		</div>		
+	</nav>
+	<!-- MAIN -->
+	<main>
+	<div class='container'>
+	
+	</div>
+	<p>Photos</p>
+	</main>
+	<!-- FOOTER -->
+	<footer class='text-center'>
+		<nav class='navbar-inverse navbar-bottom'>
+			<div class='container-fluid'>
+				<ul class='nav navbar-nav'>
+					<li class='navbar-text'>Copyight &copy; Marko Embreuš</li>
+					<li><a href='#'><i class='fab fa-facebook'></i></a></li>
+					<li><a href='#'><i class='fab fa-google-plus-square'></i></a></li>
+					<li><a href='#'><i class='fas fa-envelope'></i></a></li>
+				</ul>
+				<ul class='nav navbar-nav navbar-right'>
+					<li><a href='#'><i class='fas fa-sitemap'></i></a></li>
+					<li><a href='#'>Terms &amp; Conditions</a></li>					
+				</ul>
+			</div>
+		</nav>
+	</footer>
+	<!-- JAVASCRIPT -->
+	
+	<!--<script src="https://use.fontawesome.com/668c015d0c.js"></script>-->
+	
+	
+	<!--JQuery-->
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>	
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>	
+	<!--Custom css-->	
+</body>
+</html>
