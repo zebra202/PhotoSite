ed724b32ac4a179fe21cb4e2f686310e890a5594
zebra202 <zebra.202@hotmail.com>
2019-04-17 23:49:49 +0200

    Prilagodba index.php

--- Index.php
+++ Index.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 		<meta charset="utf-8">
@@ -6,15 +6,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Embreuš Photography</title>
 		<!--CSS-->
-		<!--Bootstrap-->
+		<!--Bootstrap 4-->
 		<!-- Latest compiled and minified CSS -->
-<!--		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<!--	 Font awsome -->		
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">		
 		<!-- Blma CSS -->
 <!--		<link rel="stylesheet" href="css/bulma.min.css"> -->
-
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/style.css">		
 </head>	
@@ -29,7 +27,7 @@
 		</div>
 	</header>
 
-	<nav class='navbar navbar-inverse navbar-fixed-top'>
+	<!--<nav class='navbar navbar-inverse navbar-fixed-top'>
 		<div class='container-fluid'>
 			<header class='navbar-header'>
 				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
@@ -56,6 +54,41 @@
 				</li>
 			</ul>
 		</div>				
+	</nav>	-->
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  		<div class='container-fluid'>
+			<!-- Toggler/collapsibe Button -->
+  			<header class='navbar-header'>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class='navbar-brand' href='index.php'>Home page</a>
+			</header>
+	  		<!-- Navbar links -->
+	  		<div class="collapse navbar-collapse" id="myNavbar">  			
+	    		<ul class="navbar-nav">
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">Portfolio</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">Contact</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">About</a>
+	      			</li> 
+	    		</ul>
+	    		
+	    		<ul class='nav navbar-nav navbar-right'>
+					<li>
+						<form class='navbar-form' action='#' method='POST'>
+							<input type='text' class='form-control' placeholder='Search the site'>
+							<button type='submit' class='btn-primary'>
+								<span class='fa fa-search'></span>
+							</button>
+					</li>
+				</ul>
+	  		</div> 
+	  	</div>
 	</nav>
 	<!-- MAIN -->
 	<main>		
@@ -106,8 +139,8 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>	
 	
 	<!-- Latest compiled and minified JavaScript -->	
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!--	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>-->
+	<script src="js/bootstrap.min.js"></script>
 	<!--Custom css-->	
 </body>
 </html>
